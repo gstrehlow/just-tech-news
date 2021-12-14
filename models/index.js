a9fd2d6442c8e776cccb02735e54649cad7043ef
gstrehlow <90304692+gstrehlow@users.noreply.github.com>
2021-12-13 20:32:44 -0600

    finished the backend

--- models/index.js
+++ models/index.js
@@ -1,3 +1,50 @@
-const User = require('./User');
+const User = require("./User");
+const Post = require("./Post");
+const Vote = require('./Vote');
+const Comment = require('./Comment');
+// create associations
+User.hasMany(Post, { //user owns post
+    foreignKey: 'user_id'
+});
+Post.belongsTo(User, { //post is owned by user
+    foreignKey: 'user_id',
+});
 
-module.exports = { User };
+User.belongsToMany(Post, { //when query post, we can see how many votes a user creates
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+Post.belongsToMany(User, { //when query user, we can see all of the posts they've voted on
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
+//force : true is like DROP TABLE IF EXISTS, use everytime you update the table associations
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
